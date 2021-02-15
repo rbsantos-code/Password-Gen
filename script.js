c33ea041b4c6754c97df7244d06a93170f775804
Your Name <richardbriansantos@Richards-MacBook-Pro.local>
2021-02-14 20:18:48 -0800

    create variables

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 // create function, variables, arrays, and code in-order to complete task
 // Assignment code here
 
+var enter;
+var containNumber;
+var containSpecial;
+var containUpper;
+var containLower;
+var outcome; // name of the password combinations
+var passwordOutcome; // ending password outcome
+
+// values used for password
+var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+
+var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
